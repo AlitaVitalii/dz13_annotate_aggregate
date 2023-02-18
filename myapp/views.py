1111d81a6e859a9626543f6d38c6b83f69ecd882
alita <alita.avs@gmail.com>
2023-02-18 23:12:56 +0200

    select/prefetch…

--- myapp/views.py
+++ myapp/views.py
@@ -32,43 +32,68 @@ def index(request):
 class BookListView(generic.ListView):
     model = Book
     paginate_by = 10
-    queryset = Book.objects.prefetch_related('authors', 'store_set', 'publisher').all().order_by('name')
+    queryset = Book.objects.prefetch_related('authors', 'store_set').select_related('publisher').order_by('name')
 
 
 class BookDetailView(generic.DetailView):
     model = Book
+    queryset = Book.objects.prefetch_related('authors').order_by('name')
 
 
 class AuthorListView(generic.ListView):
     model = Author
     paginate_by = 10
-    queryset = Author.objects.all().annotate(avg_rating=Avg('book__rating')).order_by('name')
+    queryset = Author.objects.annotate(
+        avg_rating=Avg('book__rating')
+    ).prefetch_related('book_set').order_by('name')
 
 
 class AuthorDetailView(generic.DetailView):
     model = Author
+    queryset = Author.objects.annotate(
+        num_books=Count('book'),
+        avg_price=Avg('book__price'),
+        min_price=Min('book__price'),
+        max_price=Max('book__price'),
+        avg_rating=Avg('book__rating')
+        ).prefetch_related('book_set')
 
 
 class PublisherListView(generic.ListView):
     model = Publisher
     paginate_by = 10
-    queryset = Publisher.objects.all().annotate(num_book=Count('book')).order_by('name')
+    queryset = Publisher.objects.annotate(
+        num_book=Count('book')
+    ).prefetch_related('book_set').order_by('name')
 
 
 class PublisherDetailView(generic.DetailView):
     model = Publisher
+    queryset = Publisher.objects.annotate(
+        num_books=Count('book'),
+        avg_price=Avg('book__price'),
+        min_price=Min('book__price'),
+        max_price=Max('book__price'),
+
+    ).prefetch_related('book_set')
 
 
 class StoreListView(generic.ListView):
     model = Store
     paginate_by = 10
-    queryset = Store.objects.all().annotate(
+    queryset = Store.objects.annotate(
         num_book=Count('books'),
         avg_price=Avg('books__price'),
         min_price=Min('books__price'),
         max_price=Max('books__price'),
-    ).order_by('name')
+    ).prefetch_related('books').order_by('name')
 
 
 class StoreDetailView(generic.DetailView):
     model = Store
+    queryset = Store.objects.annotate(
+        num_books=Count('books'),
+        avg_price=Avg('books__price'),
+        min_price=Min('books__price'),
+        max_price=Max('books__price'),
+    ).prefetch_related('books')
