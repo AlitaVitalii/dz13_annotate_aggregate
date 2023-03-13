6b7fb8f3b76d63c2e543a318afadc582bc5861d7
alita <alita.avs@gmail.com>
2023-03-13 23:38:31 +0200

    dz17. Django Cache

--- core/urls.py
+++ core/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import include, path
 
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url='/myapp/')),
+
     path('admin/', admin.site.urls),
 
     path('myapp/', include('myapp.urls')),
